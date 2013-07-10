5c0a3f14290954e76b2580959397d3bca36cc87e
Leo Chen <maverickcc@gmail.com>
2013-07-10 21:04:17 +0000

    renew web.js of readFileSync

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var fs = require('fs');
-    var content = fs.ReadFileSync('index.html', 'utf-8');
+    var content = fs.readFileSync('index.html', 'utf-8');
     response.send(content.toString());
 
 });
